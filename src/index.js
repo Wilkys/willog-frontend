c4b6c771091f2cfcbc146c0bbae2f0666f02f24d
seaofcalm <seaofcalm@gmail.com>
2021-06-14 22:49:28 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer, { rootSaga } from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { tempSetUser, check } from './modules/user';
+import { HelmetProvider } from 'react-helmet-async';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer, 
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if(!user) return;
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+sagaMiddleware.run(rootSaga);
+loadUser();
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
